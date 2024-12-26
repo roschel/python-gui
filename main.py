ddd49f611eeb138117758c0a06715fb2fb5aed21
roschel <joaomroschel@gmail.com>
2024-12-26 14:03:32 -0300

    Widgets: the most common

--- main.py
+++ main.py
@@ -1,19 +1,62 @@
 import sys
 
-from PySide6.QtGui import QContextMenuEvent, QAction
-from PySide6.QtWidgets import QApplication, QMainWindow, QMenu
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QVBoxLayout,
+    QTimeEdit,
+    QSpinBox,
+    QSlider,
+    QRadioButton,
+    QPushButton,
+    QCheckBox,
+    QComboBox,
+    QDateEdit,
+    QDateTimeEdit,
+    QDial,
+    QDoubleSpinBox,
+    QFontComboBox,
+    QLCDNumber,
+    QLabel,
+    QLineEdit,
+    QProgressBar,
+    QWidget
+)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-    def contextMenuEvent(self, event: QContextMenuEvent):
-        context = QMenu(self)
-        context.addAction(QAction("teste 1", self))
-        context.addAction(QAction("teste 2", self))
-        context.addAction(QAction("teste 3", self))
-        context.exec(event.globalPos())
+        self.setWindowTitle("Widgets App")
+
+        layout = QVBoxLayout()
+        widgets = [
+            QCheckBox,
+            QComboBox,
+            QDateEdit,
+            QDateTimeEdit,
+            QDial,
+            QDoubleSpinBox,
+            QFontComboBox,
+            QLCDNumber,
+            QLabel,
+            QLineEdit,
+            QProgressBar,
+            QPushButton,
+            QRadioButton,
+            QSlider,
+            QSpinBox,
+            QTimeEdit,
+        ]
+
+        for widget in widgets:
+            layout.addWidget(widget())
+
+        central_widget = QWidget()
+        central_widget.setLayout(layout)
+
+        self.setCentralWidget(central_widget)
 
 
 app = QApplication(sys.argv)
