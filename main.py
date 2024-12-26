1308ae01b8ad33d5a3a1bc055cb188dab34579a8
roschel <joaomroschel@gmail.com>
2024-12-26 10:20:01 -0300

    Signals and slots

--- main.py
+++ main.py
@@ -8,12 +8,22 @@ class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("My App")
-        button = QPushButton("Press Me!")
+        self.button_is_checked = True
 
+        self.setWindowTitle("My App")
         self.setFixedSize(QSize(400, 300))
 
-        self.setCentralWidget(button)
+        self.button = QPushButton("Press Me!")
+        self.button.clicked.connect(self.the_button_was_clicked)
+        self.button.setFixedSize(QSize(200, 150))
+
+        self.setCentralWidget(self.button)
+
+    def the_button_was_clicked(self):
+        self.button.setText("You already clicked me.")
+        self.button.setEnabled(False)
+        self.setWindowTitle("My Oneshot App")
+        print("Clicked!")
 
 
 app = QApplication(sys.argv)
