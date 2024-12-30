df4c125e35a1df592ce9742a3535195ac9667f5c
roschel <joaomroschel@gmail.com>
2024-12-30 10:23:07 -0300

    Layout: nesting with spacing

--- main.py
+++ main.py
@@ -27,6 +27,9 @@ class MainWindow(QMainWindow):
         layout2 = QVBoxLayout()
         layout3 = QVBoxLayout()
 
+        layout1.setContentsMargins(0, 0, 0, 0)
+        layout1.setSpacing(20)
+
         layout2.addWidget(Color('red'))
         layout2.addWidget(Color('yellow'))
         layout2.addWidget(Color('purple'))
