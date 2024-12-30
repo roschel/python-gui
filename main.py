c07a8edb1de2297848fe06d5c8bd48d1dba70840
roschel <joaomroschel@gmail.com>
2024-12-30 10:08:11 -0300

    Layout: criando cor com wigdet

--- main.py
+++ main.py
@@ -1,45 +1,30 @@
 import sys
 
+from PySide6.QtGui import QPalette, QColor
 from PySide6.QtWidgets import (
     QApplication,
-    QMainWindow,
-    QListWidget, QListWidgetItem, QLineEdit
+    QMainWindow, QWidget
 )
+
+
+class Color(QWidget):
+    def __init__(self, color: str):
+        super().__init__()
+        self.setAutoFillBackground(True)
+
+        palette = self.palette()
+        palette.setColor(QPalette.Window, QColor(color))
+        self.setPalette(palette)
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
 
-        self.setWindowTitle("Widgets App")
+        self.setWindowTitle("My App")
 
-        self.line_edit = QLineEdit()
-        self.line_edit.setMaxLength(10)
-        self.line_edit.setPlaceholderText("Enter yout text")
-
-        # widget.setReadOnly(True) # uncomment this to make readonly
-
-        self.line_edit.returnPressed.connect(self.return_pressed)
-        self.line_edit.selectionChanged.connect(self.selection_changed)
-        self.line_edit.textChanged.connect(self.text_changed)
-        self.line_edit.textEdited.connect(self.text_edited)
-
-        self.setCentralWidget(self.line_edit)
-
-    def text_changed(self, text):
-        print(f"Text changed to... {text}")
-
-    def return_pressed(self):
-        print("return_pressed")
-        self.line_edit.setText("BOOM!")
-
-    def selection_changed(self):
-        print("selection_changed")
-        print(self.line_edit.selectedText())
-
-    def text_edited(self, text):
-        print(f"Text edited to... {text}")
-        print(text)
+        widget = Color('red')
+        self.setCentralWidget(widget)
 
 
 app = QApplication(sys.argv)
