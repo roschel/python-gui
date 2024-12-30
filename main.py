62526da43976e9667654ef4138e1277b9963b113
roschel <joaomroschel@gmail.com>
2024-12-30 12:11:13 -0300

    actions|toolbars|menus: adding more widgets to our toolbar

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from PySide6.QtCore import Qt, QSize
 from PySide6.QtGui import QAction, QIcon
 from PySide6.QtWidgets import (
     QApplication,
-    QMainWindow, QLabel, QToolBar, QStatusBar
+    QMainWindow, QLabel, QToolBar, QStatusBar, QCheckBox
 )
 
 
@@ -24,11 +24,22 @@ class MainWindow(QMainWindow):
         toolbar.setToolButtonStyle(Qt.ToolButtonStyle.ToolButtonTextBesideIcon)
         self.addToolBar(toolbar)
 
-        button_action = QAction(QIcon('bug.png'), "Your Button", self)
+        button_action = QAction(QIcon('bug.png'), "&Your Button", self)
         button_action.setStatusTip("This is your button")
         button_action.triggered.connect(self.on_my_toolbar_button_click)
         button_action.setCheckable(True)
         toolbar.addAction(button_action)
+
+        toolbar.addSeparator()
+
+        button_action_2 = QAction(QIcon('bug.png'), "Your &Button2", self)
+        button_action_2.setStatusTip("This is your button2")
+        button_action_2.triggered.connect(self.on_my_toolbar_button_click)
+        button_action_2.setCheckable(True)
+        toolbar.addAction(button_action_2)
+
+        toolbar.addWidget(QLabel("Hello"))
+        toolbar.addWidget(QCheckBox())
 
         self.setStatusBar(QStatusBar(self))
 
