aa29f9f2a57d7b6417b9dd5e4dd292069bb28652
roschel <joaomroschel@gmail.com>
2024-12-30 10:31:09 -0300

    Layout: grid

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import sys
 from PySide6.QtGui import QPalette, QColor
 from PySide6.QtWidgets import (
     QApplication,
-    QMainWindow, QWidget, QHBoxLayout, QVBoxLayout
+    QMainWindow, QWidget, QHBoxLayout, QVBoxLayout, QGridLayout
 )
 
 
@@ -23,27 +23,15 @@ class MainWindow(QMainWindow):
 
         self.setWindowTitle("My App")
 
-        layout1 = QHBoxLayout()
-        layout2 = QVBoxLayout()
-        layout3 = QVBoxLayout()
+        layout = QGridLayout()
 
-        layout1.setContentsMargins(0, 0, 0, 0)
-        layout1.setSpacing(20)
-
-        layout2.addWidget(Color('red'))
-        layout2.addWidget(Color('yellow'))
-        layout2.addWidget(Color('purple'))
-
-        layout1.addLayout(layout2)
-        layout1.addWidget(Color('green'))
-
-        layout3.addWidget(Color('red'))
-        layout3.addWidget(Color('purple'))
-
-        layout1.addLayout(layout3)
+        layout.addWidget(Color('red'), 0, 0)
+        layout.addWidget(Color('green'), 1, 0)
+        layout.addWidget(Color('blue'), 1, 1)
+        layout.addWidget(Color('purple'), 2, 0)
 
         widget = QWidget()
-        widget.setLayout(layout1)
+        widget.setLayout(layout)
 
         self.setCentralWidget(widget)
 
